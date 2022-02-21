4a308a3807d004d4379c5a780c15f378759e8af3
MantasPtr <man.petrikas@gmail.com>
2022-02-21 20:25:14 +0200

    Type change in header

--- src/common/utils_flat.h
+++ src/common/utils_flat.h
@@ -3,7 +3,7 @@ int sqr(int x);
 
 double *empty_matrix(int matrix_dimention);
 void delete_matrix(double *matrix);
-double *generate_matrix(int matrix_dimention, int max_value);
+double *generate_matrix(int matrix_dimention, double max_value);
 
 enum matrix_prefil
 {
