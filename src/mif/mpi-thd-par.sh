d1dd08f9f4044c8297a7d334ad35f41e45e4e601
MantasPtr <man.petrikas@gmail.com>
2022-05-26 01:36:58 +0300

    tests

--- src/mif/mpi-thd-par.sh
+++ src/mif/mpi-thd-par.sh
@@ -2,9 +2,9 @@
 #SBATCH -p main
 #SBATCH -n2
 #SBATCH -D /scratch/lustre/home/mape3071/Thermodynamics/src/parallel
-module load openmpi
+# module load openmpi
 # module load mpich
 which mpicc
 which mpic++
-make compile
-make run
+# make compile
+# make run
--- src/mif/thd-gpu.sh
+++ src/mif/thd-gpu.sh
@@ -4,4 +4,4 @@
 #SBATCH -n1
 #SBATCH -D /scratch/lustre/home/mape3071/Thermodynamics/src/cuda
 
-nvidia-smi
+make
