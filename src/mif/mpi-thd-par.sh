138bedfb5491e662e4e622c35509ad9ce1283adf
MantasPtr <man.petrikas@gmail.com>
2022-05-27 23:08:09 +0300

    test

--- src/mif/mpi-thd-par.sh
+++ src/mif/mpi-thd-par.sh
@@ -2,8 +2,8 @@
 #SBATCH -p main
 #SBATCH -n2
 #SBATCH -D /scratch/lustre/home/mape3071/Thermodynamics/src/parallel
-module load openmpi
+# module load openmpi
 which mpicc
 which mpic++
-# make compile
+make compile
 # make run
